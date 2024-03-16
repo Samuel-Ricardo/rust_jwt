768490e5b981d12bc54c6017db3da0734c7f09a2
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-16 11:10:10 -0300

    [ :video_game: ] | create: controller - public_view_handler (app::controller)

--- axum-auth/src/controller/mod.rs
+++ axum-auth/src/controller/mod.rs
@@ -2,11 +2,28 @@ use axum::{
     http::{header, StatusCode},
     response::Response,
 };
+use serde_json::json;
 
 pub async fn hello_world() -> Response<String> {
     Response::builder()
         .status(StatusCode::OK)
         .header(header::CONTENT_TYPE, "application/json")
         .body("{\"message\": \"Hello, World!\"}".to_string())
+        .unwrap_or_default()
+}
+
+pub async fn public_view_handler() -> Response<String> {
+    Response::builder()
+        .status(StatusCode::OK)
+        .header(header::CONTENT_TYPE, "application/json")
+        .body(
+            json!({
+                "success": true,
+                "data": {
+                    "message": "This data is visible to all users"
+                }
+            })
+            .to_string(),
+        )
         .unwrap_or_default()
 }
