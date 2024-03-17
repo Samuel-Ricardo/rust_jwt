9857bb79a8d6a989d19b093e12b21c475be59956
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-17 12:29:55 -0300

    [ :video_game: ] | create: controller - secret_view_handler (app::controller)

--- actix-auth/src/controller/auth.rs
+++ actix-auth/src/controller/auth.rs
@@ -2,6 +2,8 @@ use actix_web::{web::Json, HttpResponse};
 use jwt_lib::model::user::User;
 use serde_json::json;
 
+use crate::middleware::auth::Auth;
+
 pub async fn get_token_handler(Json(user): Json<User>) -> HttpResponse {
     let token = jwt_lib::get_jwt_secret(user);
 
@@ -20,4 +22,11 @@ pub async fn get_token_handler(Json(user): Json<User>) -> HttpResponse {
           }
         })),
     }
+}
+
+pub async fn secret_view_handler(Auth(user): Auth) -> HttpResponse {
+    HttpResponse::Ok().json(json!({
+        "success": true,
+        "data": user
+    }))
 }
