8aed4384b6d5d4aada8318c8647955f9cb8a1c9e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-17 09:21:21 -0300

    [ :vertical_traffic_light: ] | map: route - secret)_view (app::server)

--- axum-auth/src/server.rs
+++ axum-auth/src/server.rs
@@ -4,7 +4,7 @@ use axum::{
 };
 use tokio::net::TcpListener;
 
-use crate::controller::auth::get_token_handler;
+use crate::controller::auth::{get_token_handler, secret_view_handler};
 use crate::controller::{hello_world, public_view_handler};
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 6)]
@@ -12,7 +12,8 @@ pub async fn startup() {
     let routes = Router::new()
         .route("/", get(hello_world))
         .route("/public-view", get(public_view_handler))
-        .route("/get-token", post(get_token_handler));
+        .route("/get-token", post(get_token_handler))
+        .route("/secret-view", get(secret_view_handler));
 
     let tcp_listener = TcpListener::bind("127.0.0.1:2323")
         .await
