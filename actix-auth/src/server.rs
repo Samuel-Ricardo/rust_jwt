276ef9e801c4cf30328aa2cc337a372db502417b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-17 11:53:26 -0300

    [ :vertical_traffic_light: ] | map: route - public_view (app::server)

--- actix-auth/src/server.rs
+++ actix-auth/src/server.rs
@@ -1,13 +1,17 @@
 use actix_web::{web::get, App, HttpServer};
 
-use crate::controller::hello_world;
+use crate::controller::{hello_world, public_view_handler};
 
 #[actix_web::main]
 pub async fn setup() -> std::io::Result<()> {
-    HttpServer::new(move || App::new().route("/", get().to(hello_world)))
-        .workers(4)
-        .bind("127.0.0.1:2424")
-        .expect("Address should be free and valid")
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .route("/", get().to(hello_world))
+            .route("/public-view", get().to(public_view_handler))
+    })
+    .workers(4)
+    .bind("127.0.0.1:2424")
+    .expect("Address should be free and valid")
+    .run()
+    .await
 }
